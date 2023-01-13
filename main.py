e2e20579901d9a5a847356250586961cb727c0db
Ludek Fedurca <fedurca@gmail.com>
2023-01-13 09:02:34 +0100

    final desc and price parser

--- main.py
+++ main.py
@@ -3,25 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 import sys 
 
-page = requests.get('https://www.bazos.cz/search.php?hledat=mazda+mx5&hlokalita=&humkreis=25&cenaod=10000&cenado=&order=1')
+#page = requests.get('https://www.bazos.cz/search.php?hledat=mazda+mx5&hlokalita=&humkreis=25&cenaod=10000&cenado=&order=1')
 # Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
+
+with open("/home/fedurca/Documents/webreader/webreader/ad.html") as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+
+#soup = BeautifulSoup(page.text, 'html.parser')
 # get the repo list
 # repo = soup.find(class_="repo-list")
 
 #print(type(soup))
 #print(soup.)
 
-nadpis = soup.find_all(class_="nadpis")
+inzeraty = soup.find_all(class_="inzeraty inzeratyflex") 
 
-cena = soup.find_all(class_="inzeratycena")
+#nadpis = soup.find_all(class_="nadpis")
 
-for record in cena:
-    print(record.get_text())
+#cena = soup.find_all(class_="inzeratycena")
 
-for record in nadpis:
-    print(record.get_text())
-
+for tag in inzeraty:
+    
+    nadpis = soup.find(class_='nadpis')
+    #print(type(nadpis))
+    #<class 'bs4.element.Tag'>
+    nadpis_plain = nadpis.get_text().strip()
+    print(nadpis_plain)
+    
+    cena = soup.find(class_='inzeratycena')
+    cena_plain = cena.get_text().strip()
+    print(cena_plain)
+    #print(record.contents[3])
+    #print(tag.contents[1])
+    #print(next(tag.children).strip())
+    #print(next(tag.strings).strip())
+    #print(next(tag.stripped_strings))
+    break
     # record.contents[0]
 #print(repo)
 
